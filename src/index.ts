86d11e69a34a2b447760a2b47768571d24610cb8
unknown <petratech1830@gmail.com>
2026-05-01 00:56:02 +0700

    feat: add profile switcher command with list, current, and switch actions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { Command } from "commander";
 import { validateCommand } from "./commands/validate.js";
+import { useCommand } from "./commands/use.js";
 
 const program = new Command();
 
@@ -11,5 +12,6 @@ program
   .version("1.1.0");
 
 program.addCommand(validateCommand);
+program.addCommand(useCommand);
 
 program.parse(process.argv);
